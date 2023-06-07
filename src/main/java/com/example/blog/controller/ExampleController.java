4f5e03bf3d42b12ee48047ed41ee96d3da283b10
GiJinNam <87312828+GiJinNam@users.noreply.github.com>
2023-06-07 15:45:04 +0900

    블로그 메인화면 완성 (글 목록만 보여주는거)

---
+++ src/main/java/com/example/blog/controller/ExampleController.java
@@ -0,0 +1,37 @@
+package com.example.blog.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class ExampleController {
+
+    @GetMapping("/thymeleaf/example")
+    public String thymeleafExample(Model model) {
+        Person examplePerson = new Person();
+        examplePerson.setId(1L);
+        examplePerson.setName("남기진");
+        examplePerson.setAge(98);
+        examplePerson.setHobbies(List.of("응애","응애에요"));
+
+        model.addAttribute("person",examplePerson);
+        model.addAttribute("today", LocalDate.now());
+
+        return "example";
+    }
+
+    @Getter
+    @Setter
+    class Person {
+        private Long id;
+        private String name;
+        private int age;
+        private List<String> hobbies;
+    }
+}
